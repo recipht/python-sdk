3038f2a050d7fbab14ec61861db2cee963ec1a8d
Abdulhakeem Mustapha <abdulhakeemmustapha@gmail.com>
2025-10-23 13:45:11 +0100

    feat: populate main exports

---
+++ src/receiptrail/__init__.py
@@ -0,0 +1,46 @@
+"""Receiptrail SDK for Python"""
+
+from .client import ReceiptrailClient
+from .types.ingestor import (
+    IngestReceiptRequest,
+    IngestReceiptResponse,
+    ReceiptValueResponse,
+    ReceiptCountResponse,
+    ReceiptChartResponse,
+    SuccessRateResponse,
+)
+from .types.normalizer import (
+    ProcessImageRequest,
+    ProcessImageResponse,
+    ProcessJsonRequest,
+    ProcessBulkJsonRequest,
+    NormalizedReceipt,
+    LineItem,
+)
+from .types.delivery import (
+    DeliveriesSummaryResponse,
+    DeliverySuccessRateResponse,
+    DeliveryChartResponse,
+    DeliveryChartDataPoint,
+)
+
+__version__ = "0.1.0"
+__all__ = [
+    "ReceiptrailClient",
+    "IngestReceiptRequest",
+    "IngestReceiptResponse",
+    "ReceiptValueResponse",
+    "ReceiptCountResponse",
+    "ReceiptChartResponse",
+    "SuccessRateResponse",
+    "ProcessImageRequest",
+    "ProcessImageResponse",
+    "ProcessJsonRequest",
+    "ProcessBulkJsonRequest",
+    "NormalizedReceipt",
+    "LineItem",
+    "DeliveriesSummaryResponse",
+    "DeliverySuccessRateResponse",
+    "DeliveryChartResponse",
+    "DeliveryChartDataPoint",
+]
