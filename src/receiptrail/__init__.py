42d996d3331b6c62db57e290ddce7639ebc7946d
Abdulhakeem Mustapha <abdulhakeemmustapha@gmail.com>
2025-10-23 22:33:00 +0100

    feat: export ReceiptrailConfig type

--- src/receiptrail/__init__.py
+++ src/receiptrail/__init__.py
@@ -1,6 +1,7 @@
 """Receiptrail SDK for Python"""
 
 from .client import ReceiptrailClient
+from .types import ReceiptrailConfig
 from .types.ingestor import (
     IngestReceiptRequest,
     IngestReceiptResponse,
@@ -27,6 +28,7 @@ from .types.delivery import (
 __version__ = "0.1.0"
 __all__ = [
     "ReceiptrailClient",
+    "ReceiptrailConfig",
     "IngestReceiptRequest",
     "IngestReceiptResponse",
     "ReceiptValueResponse",
